5eef0b16faa7bea75d780e48196437be0a4ad96f
Ziyu <ziyuye@uchicago.edu>
2025-04-23 22:01:29 -0500

    update: model upload

---
+++ scripts/upload_hf.sh
@@ -0,0 +1,3 @@
+python model_merger.py \
+    --local_dir ~/github/code-r1/models/1-epoch-off-checkpoint/global_step_64/actor \
+    --hf_upload_path cat-searcher/qwen-2.5-7B-1M-code-r1-epoch-1-step-64
